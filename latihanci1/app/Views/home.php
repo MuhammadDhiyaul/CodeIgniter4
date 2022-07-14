3d0dca51f7f8cf9e8cd6921b381844000d2435b7
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2022-07-14 15:53:56 +0700

    Instalasi CI 07/14/2022

---
+++ latihanci1/app/Views/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codeigniter 4 - Latihan</title>
+</head>
+<body align="center">
+    <h1>Hallo Teman - Teman!</h1>
+
+    <p>Nama saya Muhammad Dhiyaul Auliya. Ini adalah halaman pertama yang berisi biodata pribadi saya</p>
+    
+    <table border="2" align="center">
+        <tr>
+            <th>Nama</th>
+            <th> : </th>
+            <th>Muhammad Dhiyaul Auliya</th>
+        </tr>
+        <tr>
+            <th>Alamat</th>
+            <th> : </th>
+            <th>Jl Sultan Agung No 48 RT06 RT03</th>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <th> : </th>
+            <th>Laki - Laki</th>
+        </tr>
+        <tr>
+            <th>Universitas</th>
+            <th> : </th>
+            <th>Politeknik Negeri Malang</th>
+        </tr>
+    </table>
+    <br>
+
+    <p>Untuk mengetahui tentang project saya, bisa klik link dibawah ini !</p>
+
+    <a href="/home2">Menuju Halaman 2</a>
+</body>
+</html>
---
+++ latihanci1/app/Views/home2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codeigniter 4 - Latihan</title>
+</head>
+<body align="center">
+    <h1>Hallo Teman - Teman !</h1>
+
+    <p>Ini adalah halaman 2. Kumpulan project yang sudah saya kerjakan selama saya kuliah di Politeknik Negeri Malang.</p>
+
+    <table border="2" align="center">
+    <tr>
+            <th>Pemesanan Pizza (Laravel)</th>
+            <th> : </th>
+            <th><a href="https://github.com/MuhammadDhiyaul/Aplikasi-Web-Order-Pizza">Pemesanan Pizza</a></th>
+        </tr>
+        <tr>
+            <th>Resep Makanan (Flutter)</th>
+            <th> : </th>
+            <th><a href="https://github.com/MuhammadDhiyaul/Aplikasi-Resep-Makanan---Flutter">Resep Makanan</a></th>
+        </tr>
+        <tr>
+            <th>Point Of Sale (Laravel)</th>
+            <th> : </th>
+            <th><a href="https://github.com/MuhammadDhiyaul/Aplikasi-PointOfSales---Laravel--">Point Of Sale</a></th>
+        </tr>
+        <tr>
+            <th>Sistem Pendukung Keputusan Pemilihan Tempat Usaha - Metode SAW</th>
+            <th> : </th>
+            <th><a href="https://github.com/MuhammadDhiyaul/Sistem-Pendukung-Keputusan-Pemilihan-Lokasi-Usaha---Metode-SAW">Sistem Pendukung Keputusan Pemilihan Tempat Usaha</a></th>
+        </tr>
+    </table>
+    <br>
+
+    <p>Untuk kembali ke halaman 1, klik link dibawah ini !</p>
+
+    <a href="/">Menuju Halaman 1</a>
+</body>
+</html>
